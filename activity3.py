97d27cf020a4f21d199c461c83387d3b32f2379f
jmmalijana75-cyber <jmmalijana75@gmail.com>
2026-08-23 17:09:07 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,56 @@
+#int
+age = 18
+score = 95
+siblings = 2
+year = 2026
+
+print("My age is", age)
+
+print("My score is", score)
+
+print("I have", siblings, "siblings")
+
+print("The year is", year)
+
+#float
+temperature = 36.1
+height = 5.8
+price = 99.50
+grade = 87.5
+
+print("My height is", height, "feet")
+
+print("The price is ₱", price)
+
+print("My grade is", grade)
+
+print("The temperature is", temperature, "°C")
+
+#string
+name = "Jm"
+school = "DLL"
+food = "Chicken"
+city = "Lucena"
+
+print("My name is", name)
+
+print("I study in", school)
+
+print("My favorite food is", food)
+
+print("I live in", city)
+
+#boolean
+student = True
+passed = True
+absent = False
+graduated = False
+
+print("I am a student:", student)
+
+print("I passed the exam:", passed)
+
+print("I am absent today:", absent)
+
+print("I am graduated:", graduated)
+

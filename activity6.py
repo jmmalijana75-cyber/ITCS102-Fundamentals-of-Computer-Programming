a0f59220a439456b4f02c9a8595815d77d8d6724
jmmalijana75-cyber <jmmalijana75@gmail.com>
2026-09-04 10:42:19 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,9 @@
+#activity 6 
+#dynamic, static
+#dynamic - ever changing 
+#static - fixed
+
+name = input("What is your name? -->")
+print("\tHello Welcome to BSIT1B", name)
+age = input("How old are you?-->")
+

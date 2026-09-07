91bf43f2990f2c430fa85d371b2fe1ee83a1b053
jmmalijana75-cyber <jmmalijana75@gmail.com>
2026-09-07 11:24:38 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,13 @@
+#basic if else program  
+
+username = "Malijana"
+password = "HelloWord!!"
+
+u = input("Input USERNAME --->")
+p = input("Input PASSWORD --->")
+
+if u == username and p == password :
+        print("username and password correct")
+
+else: 
+        print("access denied")

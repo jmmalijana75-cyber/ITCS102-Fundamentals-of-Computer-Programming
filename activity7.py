9a5dd06f483cac88efa9ada73a61bfeb7999e1b0
jmmalijana75-cyber <jmmalijana75@gmail.com>
2026-09-07 10:58:57 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,34 @@
+total = 10
+
+
+n = eval(input("enter any number ---> "))
+total = total + n 
+print("Current total is --->>" , total)
+
+b = eval(input("enter any number ---> "))
+total -= b
+print("Current total is --->>" , total)
+
+t = eval(input("enter any number ---> "))
+total *= t 
+print("Current total is --->>" , total)
+
+v = eval(input("enter any number ---> "))
+total += v
+print("Current total is --->>" , total)
+
+u = eval(input("enter any number ---> "))
+total -= u
+print("Current total is --->>" , total)
+
+o = eval(input("enter any number ---> "))
+total *= o
+print("Current total is --->>" , total)
+
+i = eval(input("enter any number ---> "))
+total += i
+print("Current total is --->>" , total)
+
+p = eval(input("enter any number ---> "))
+total /= p
+print("Current total is --->>" , total)

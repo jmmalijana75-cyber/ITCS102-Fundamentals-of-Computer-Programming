d0accd54803772a6ef1d4cff49b4804c88d713d6
jmmalijana75-cyber <jmmalijana75@gmail.com>
2026-09-11 11:22:04 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,17 @@
+# > - greater than
+# < - less than
+# >= greater than or equal to
+# <= less than or equal to
+# != not equal
+# == equal to
+
+x = 10
+y = 25
+
+
+
+print ( x > y )
+print ( x >= y )
+print ( x <= y )
+print ( x != y)
+print ( x == y)	

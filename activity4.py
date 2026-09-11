27970c5b7900926c612f47151b5d7dce625eb931
jmmalijana75-cyber <jmmalijana75@gmail.com>
2026-09-11 11:19:21 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,13 @@
+#escape sequence
+# \n - new line
+# \t - tab
+# \r - carriage return/ deprecated/obsolete
+# \b - backspace
+# \\ - insert slash
+# \" - insert double quotation
+
+phrase1 = "\"hello po '\bpabili pong tinapay \npwede \t po ba"
+phrase2 = "spideran \"in the\t\n spider verse"
+
+print(phrase1)
+print(phrase2)

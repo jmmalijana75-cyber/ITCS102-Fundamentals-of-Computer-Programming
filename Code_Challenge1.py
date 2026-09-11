3e5bae23015fd713bfeadce272aa31d628b1c8bf
jmmalijana75-cyber <jmmalijana75@gmail.com>
2026-09-11 11:15:50 +0800

    Add files via upload

---
+++ Code_Challenge1.py
@@ -0,0 +1,12 @@
+#escape sequence
+
+# \n
+# \t
+# \r
+# \b
+# \\
+# \"
+
+phrase1 = "\t\t\t\t\t\t\t\t\t\t***\n\t\t\t\t\t\t\t\t\t***\t\t***\n\t\t\t\t\t\t\t\t***\t\t\t\t***\n\t\t\t\t\t\t\t***\t\t\t\t\t\t***\n\t\t\t\t\t\t***\t\t\t\t\t\t\t\t***\n\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t***\n\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t***\n\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t***\n\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t***\n\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t***\n\t\t\t\t\t***\t\t\tThis is JOHN MEYNARD B. MALIJANA\t\t\t***\n\t\t\t\t\t***\t\t\t\tfrom BSIT-1B\t\t\t\t\t***\n\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t***\n\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t***\n\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t***\n\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t***\n\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t***\n\t\t\t\t\t***\t\t\t\t\t\t\t\t\t\t***\n\t\t\t\t\t***********************************************************************************\n\t\t\t\t\t***********************************************************************************\n\t\t\t\t\t***********************************************************************************"
+
+print(phrase1)
